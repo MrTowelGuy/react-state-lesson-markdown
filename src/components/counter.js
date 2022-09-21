96c0d275de538b670f04f40e28ab2d6e0a5c2ab3
Jaden Garrity <pjaden.garrity@gmail.com>
2022-09-21 15:18:12 +0000

    commit 1

--- src/components/counter.js
+++ src/components/counter.js
@@ -7,12 +7,25 @@ function Counter() {
   // const [name, setName] = useState("Jaden");
   // const [health, setHealth] = useState(100);
 
+// old way
+// const countState = useState[0];
+// const count = countState[0];
+// const setCount = countState[1]
+// console.log("countState -", countState)
+
+//array deconstructor
+const [count, setCount] = useState(0);
+// const handleClick= ()=>{alert("you clicked")}
+
+const add = ()=> setCount(count + 1);
+const sub = ()=> setCount(count - 1);
+
   return (
     <>
-      <span>Current Count: 0</span>
+      <span>Current Count: {count}</span>
       <section>
-        <button>+</button>
-        <button>-</button>
+        <button onClick={add}>+</button>
+        <button onClick={sub}>-</button>
       </section>
     </>
   );
